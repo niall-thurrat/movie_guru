9347991005f9570e2caa336da027cec5834cc182
Niall Thurrat <nt222fc@student.lnu.se>
2020-07-29 20:12:32 +0200

    package.json and app.js added

---
+++ app.js
@@ -0,0 +1,37 @@
+/**
+ * The starting point of the application.
+ *
+ * @author Niall Thurrat
+ * @version 1.0.0
+ */
+
+'use strict'
+
+const express = require('express')
+const hbs = require('express-hbs')
+const path = require('path')
+const logger = require('morgan')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// view engine config
+app.engine('hbs', hbs.express4({
+  defaultLayout: path.join(__dirname, 'views', 'layouts', 'default')
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+// middleware
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger('dev'))
+app.use(express.urlencoded({ extended: false }))
+
+// routes
+app.use('/', require('./routes/homeRouter'))
+app.use('/my-page', require('./routes/myPageRouter'))
+app.use('/rate-movies', require('./routes/usersRouter'))
+app.use('/users', require('./routes/usersRouter'))
+
+// run server
+app.listen(port, () => console.log(`Server running on port ${port}`))
