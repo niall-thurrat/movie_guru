0816ef3ff249e21c4e303d9510a2a8416608de52
Niall Thurrat <nt222fc@student.lnu.se>
2020-08-14 12:39:15 +0200

    query3 added to home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -34,13 +34,24 @@ homeController.get = async (req, res, next) => {
     // Get 10 movies with the highest average rating in
     // the last week (last 7 days and today)
     const query2 =
-    'SELECT title, AVG(rating) AS avg_rating, COUNT(rating) AS ratings_count ' +
-    'FROM rates ' +
-    'JOIN movies ON rates.movieID = movies.ID ' +
-    'WHERE date >= DATE(NOW()) - INTERVAL 7 DAY ' +
-    'GROUP BY movieID ' +
-    'ORDER BY avg_rating DESC ' +
-    'LIMIT 10'
+      'SELECT title, AVG(rating) AS avg_rating, COUNT(rating) AS ratings_count ' +
+      'FROM rates ' +
+      'JOIN movies ON rates.movieID = movies.ID ' +
+      'WHERE date >= DATE(NOW()) - INTERVAL 7 DAY ' +
+      'GROUP BY movieID ' +
+      'ORDER BY avg_rating DESC ' +
+      'LIMIT 10'
+
+    // Get the top 10 directors whose movies have
+    // the best average rating
+    const query3 =
+      'SELECT director_name, AVG(avg_rating) AS dir_avg_rating, SUM(ratings_count) AS ratings_count ' +
+      'FROM directs ' +
+      'JOIN movies ON directs.movieID = movies.ID ' +
+      'JOIN directors ON directs.directorID = directors.ID ' +
+      'GROUP BY directorID ' +
+      'ORDER BY dir_avg_rating DESC ' +
+      'LIMIT 10'
 
     await db.query(query1, function (error, results, fields) {
       if (error) throw error
@@ -50,6 +61,11 @@ homeController.get = async (req, res, next) => {
     await db.query(query2, function (error, results, fields) {
       if (error) throw error
       locals.data2 = results
+    })
+
+    await db.query(query3, function (error, results, fields) {
+      if (error) throw error
+      locals.data3 = results
     })
 
     res.status(200)
