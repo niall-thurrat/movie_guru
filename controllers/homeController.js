64aebffe6e1eda9798df120420a769dd935561d9
Niall Thurrat <nt222fc@student.lnu.se>
2020-08-14 13:01:59 +0200

    query4 added to home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -23,18 +23,18 @@ homeController.get = async (req, res, next) => {
   try {
     const locals = { isAuthenticated: false }
 
-    // Get the 10 movies with the highest average rating
+    // Gets 10 movies with the highest average rating
     const query1 =
-      'SELECT title, avg_rating, ratings_count ' +
+      'SELECT title, ROUND( avg_rating,1 ) AS avg_rating, ratings_count ' +
       'FROM movies ' +
       'GROUP BY ID ' +
       'ORDER BY avg_rating DESC ' +
       'LIMIT 10'
 
-    // Get 10 movies with the highest average rating in
+    // Gets 10 movies with the highest average rating in
     // the last week (last 7 days and today)
     const query2 =
-      'SELECT title, AVG(rating) AS avg_rating, COUNT(rating) AS ratings_count ' +
+      'SELECT title, ROUND( avg_rating,1 ) AS avg_rating, COUNT(rating) AS ratings_count ' +
       'FROM rates ' +
       'JOIN movies ON rates.movieID = movies.ID ' +
       'WHERE date >= DATE(NOW()) - INTERVAL 7 DAY ' +
@@ -42,15 +42,26 @@ homeController.get = async (req, res, next) => {
       'ORDER BY avg_rating DESC ' +
       'LIMIT 10'
 
-    // Get the top 10 directors whose movies have
+    // Gets the top 10 directors whose movies have
     // the best average rating
     const query3 =
-      'SELECT director_name, AVG(avg_rating) AS dir_avg_rating, SUM(ratings_count) AS ratings_count ' +
+      'SELECT director_name, ROUND( AVG(avg_rating),1 ) AS dir_avg_rating, SUM(ratings_count) AS ratings_count ' +
       'FROM directs ' +
       'JOIN movies ON directs.movieID = movies.ID ' +
       'JOIN directors ON directs.directorID = directors.ID ' +
       'GROUP BY directorID ' +
       'ORDER BY dir_avg_rating DESC ' +
+      'LIMIT 10'
+
+    // Gets the top 10 actors whose movies have the
+    // best average rating
+    const query4 =
+      'SELECT actor_name, ROUND( AVG(avg_rating),1 ) AS act_avg_rating, SUM(ratings_count) AS ratings_count ' +
+      'FROM stars_in ' +
+      'JOIN movies ON stars_in.movieID = movies.ID ' +
+      'JOIN actors ON stars_in.actorID = actors.ID ' +
+      'GROUP BY actorID ' +
+      'ORDER BY act_avg_rating DESC ' +
       'LIMIT 10'
 
     await db.query(query1, function (error, results, fields) {
@@ -66,6 +77,11 @@ homeController.get = async (req, res, next) => {
     await db.query(query3, function (error, results, fields) {
       if (error) throw error
       locals.data3 = results
+    })
+
+    await db.query(query4, function (error, results, fields) {
+      if (error) throw error
+      locals.data4 = results
     })
 
     res.status(200)
