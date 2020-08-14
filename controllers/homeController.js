0431b646a54a23946c293d424c137d0733b68fad
Niall Thurrat <nt222fc@student.lnu.se>
2020-08-14 12:14:34 +0200

    query2 added to home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -19,26 +19,41 @@ const homeController = {}
  * @param {Function} next - next middleware func
  *
  */
-homeController.get = (req, res, next) => {
+homeController.get = async (req, res, next) => {
   try {
+    const locals = { isAuthenticated: false }
+
+    // Get the 10 movies with the highest average rating
     const query1 =
-        'SELECT title, avg_rating, ratings_count ' +
-        'FROM movies ' +
-        'GROUP BY ID ' +
-        'ORDER BY avg_rating DESC ' +
-        'LIMIT 10'
+      'SELECT title, avg_rating, ratings_count ' +
+      'FROM movies ' +
+      'GROUP BY ID ' +
+      'ORDER BY avg_rating DESC ' +
+      'LIMIT 10'
 
-    db.query(query1, function (error, results, fields) {
+    // Get 10 movies with the highest average rating in
+    // the last week (last 7 days and today)
+    const query2 =
+    'SELECT title, AVG(rating) AS avg_rating, COUNT(rating) AS ratings_count ' +
+    'FROM rates ' +
+    'JOIN movies ON rates.movieID = movies.ID ' +
+    'WHERE date >= DATE(NOW()) - INTERVAL 7 DAY ' +
+    'GROUP BY movieID ' +
+    'ORDER BY avg_rating DESC ' +
+    'LIMIT 10'
+
+    await db.query(query1, function (error, results, fields) {
       if (error) throw error
-
-      const locals = {
-        movies: results,
-        isAuthenticated: false
-      }
-
-      res.status(200)
-      res.render('home', { locals })
+      locals.data1 = results
     })
+
+    await db.query(query2, function (error, results, fields) {
+      if (error) throw error
+      locals.data2 = results
+    })
+
+    res.status(200)
+    res.render('home', { locals })
   } catch (error) {
     next(error)
   }
