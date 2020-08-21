bc56ef6f2af2f82b4ff099eff19d384386d1c70e
Niall Thurrat <nt222fc@student.lnu.se>
2020-08-21 14:48:24 +0200

    a-z search handled

--- controllers/rateController.js
+++ controllers/rateController.js
@@ -24,20 +24,28 @@ const rateController = {}
  */
 rateController.get = (req, res, next) => {
   try {
+    const user = req.session.user
     const locals = {
-      isAuthenticated: req.session.isAuthenticated
+      isAuthenticated: req.session.isAuthenticated,
+      alphabet: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     }
 
+    // handle keyword search for movies
     if (req.query.search) {
       const keywords = parseWords(req.query.search)
       locals.keywords = keywords
 
-      console.log(`KEYWORDS: ${keywords}`)
-
-      // query to get movies using keyword
+      // query to get movies using keywords (also
+      // joins ratings of a specific user)
       let qs =
-      'SELECT ID, title, year, avg_rating ' +
+      'SELECT ID, title, year, avg_rating AS average_rating, rating AS my_rating ' +
       'FROM movies ' +
+      'LEFT JOIN ( ' +
+        'SELECT viewerID, movieID, rating ' +
+        'FROM rates ' +
+        `WHERE viewerID = ${user.ID}) AS my_rating ` +
+      'ON movies.ID = my_rating.movieID ' +
       `WHERE title LIKE '%${keywords[0]}%' `
 
       // add to query if multiple keywords
@@ -54,8 +62,32 @@ rateController.get = (req, res, next) => {
       }
 
       db.query(qs, function (err, results, fields) {
-        if (err) return next(createError(500, 'Error in query to db'))
+        if (err) {
+          return next(createError(500, 'Error in query to db'))
+        }
+        locals.data = results
+      })
+    } else if (req.query['a-z']) {
+      console.log('this fires')
+      const index = req.query['a-z']
+      locals.index = index
 
+      // query to get movies by first letter (also
+      // joins ratings of a specific user)
+      const qs =
+      'SELECT ID, title, year, length, certificate, avg_rating AS average_rating, rating AS my_rating ' +
+      'FROM movies ' +
+      'LEFT JOIN ( ' +
+        'SELECT viewerID, movieID, rating ' +
+        'FROM rates ' +
+        `WHERE viewerID = ${user.ID}) AS my_rating ` +
+      'ON movies.ID = my_rating.movieID ' +
+      `WHERE title LIKE '${index}%'`
+
+      db.query(qs, function (err, results, fields) {
+        if (err) {
+          return next(createError(500, 'Error in query to db'))
+        }
         locals.data = results
       })
     }
